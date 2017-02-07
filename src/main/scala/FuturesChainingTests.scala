4561ef76c87b42f1dd63a03ffe76bcff8e1a24c8
Piotr Tarsa <piotr.tarsa@gmail.com>
2017-02-08 00:23:17 +0100

    Testing under Scala 2.12

--- src/main/scala/FuturesChainingTests.scala
+++ src/main/scala/FuturesChainingTests.scala
@@ -71,6 +71,27 @@ object FlatMapFixed extends ArraySetup with App {
   StdIn.readLine()
 }
 
+object FlatMapCurrent extends ArraySetup with App {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+
+  def loop(i: Int, arraySize: Int): Future[Unit] = {
+    val array = new Array[Byte](arraySize)
+    Future.successful(i).flatMap { i =>
+      if (i == 0) {
+        Future.successful(())
+      } else {
+        array.length // Force closure to refer to array
+        loop(i - 1, arraySize)
+      }
+
+    }
+  }
+
+  loop(tooManyArrays, arraySize) onComplete println
+  StdIn.readLine()
+}
+
 object RecoverWithBefore extends ArraySetup with App {
   import futures.before.concurrent.ExecutionContext.Implicits.global
   import futures.before.concurrent.Future
@@ -116,6 +137,27 @@ object RecoverWithAfter extends ArraySetup with App {
 object RecoverWithFixed extends ArraySetup with App {
   import futures.fixed.concurrent.ExecutionContext.Implicits.global
   import futures.fixed.concurrent.Future
+
+  // An example of possible space leak using ACPS by virtue of `recoverWith`:
+  def loopRW(i: Int, arraySize: Int): Future[Unit] = {
+    val array = new Array[Byte](arraySize)
+    Future(throw new Exception).recoverWith { case _ =>
+      if (i == 0) {
+        Future(())
+      } else {
+        array.length // Force closure to refer to array
+        loopRW(i - 1, arraySize)
+      }
+    }
+  }
+
+  loopRW(tooManyArrays, arraySize) onComplete println
+  StdIn.readLine()
+}
+
+object RecoverWithCurrent extends ArraySetup with App {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
 
   // An example of possible space leak using ACPS by virtue of `recoverWith`:
   def loopRW(i: Int, arraySize: Int): Future[Unit] = {
